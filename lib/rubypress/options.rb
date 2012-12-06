5c17ecd417194e09d4f06b7d720bbde8c0b35ee5
Sebastian Johnsson <sebastian@agiley.se>
2012-12-06 22:58:43 +0100

    Add set_options

--- lib/rubypress/options.rb
+++ lib/rubypress/options.rb
@@ -1,6 +1,7 @@
 module Rubypress
   module Options
     
+    #http://codex.wordpress.org/XML-RPC_WordPress_API/Options#wp.getOptions
     def get_options(options = {})
       opts = {
         :blog_id => 0,
@@ -8,8 +9,27 @@ module Rubypress
         :password => self.password,
         :options => []
       }.merge(options)
+      
       self.connection.call(
         "wp.getOptions", 
+        opts[:blog_id], 
+        opts[:username],
+        opts[:password],
+        opts[:options]
+      )
+    end
+    
+    #http://codex.wordpress.org/XML-RPC_WordPress_API/Options#wp.setOptions
+    def set_options(options = {})
+      opts = {
+        :blog_id => 0,
+        :username => self.username,
+        :password => self.password,
+        :options => {}
+      }.merge(options)
+      
+      self.connection.call(
+        "wp.setOptions", 
         opts[:blog_id], 
         opts[:username],
         opts[:password],
