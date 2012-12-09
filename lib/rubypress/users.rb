262ea7af1860d1353af6bc96b47960905b73be21
Sebastian Johnsson <sebastian@agiley.se>
2012-12-09 23:20:09 +0100

    Add support for wp.editProfile

--- lib/rubypress/users.rb
+++ lib/rubypress/users.rb
@@ -8,10 +8,29 @@ module Rubypress
         :username => self.username,
         :password => self.password
       }.merge(options)
+      
       self.connection.call(
         "wp.getUsersBlogs", 
         opts[:username],
         opts[:password]
+      )
+    end
+    
+    #http://codex.wordpress.org/XML-RPC_WordPress_API/Users#wp.editProfile
+    def edit_profile(options = {})
+      opts = {
+        :blog_id  =>  0,
+        :username =>  self.username,
+        :password =>  self.password,
+        :options  =>  {}
+      }.merge(options)
+      
+      self.connection.call(
+        "wp.editProfile", 
+        opts[:blog_id], 
+        opts[:username],
+        opts[:password],
+        opts[:options]
       )
     end
     
